c81a5aa273b43921a80c39b7ae0260c0e8c1f459
Silrojua22 <silviojuarez60@gmail.com>
2025-07-14 22:19:13 -0300

    Membership Service ok

---
+++ auth-service/index.js
@@ -0,0 +1,17 @@
+import app from "./src/app.js"; // Adjust the path as necessary
+import connectDB from "./src/config/database.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  });
