59e5c2d8fcf78c45e4c56fe583517a6446daa5a4
Silrojua22 <silviojuarez60@gmail.com>
2025-07-14 22:49:13 -0300

    Deploy auth service

--- auth-service/src/config/database.js
+++ auth-service/src/config/database.js
@@ -1,3 +1,32 @@
+//Conexion local
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// async function connectDB() {
+//   dotenv.config();
+//   const mongoURI = process.env.MONGO_URI_AUTH;
+
+//   try {
+//     const connection = await mongoose.connect(mongoURI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(
+//       "MongoDB (Auth Service) conected to:",
+//       connection.connection.host,
+//       connection.connection.port,
+//       connection.connection.name
+//     );
+//     return connection.connection;
+//   } catch (error) {
+//     console.error("MongoDB connection error (Auth Service):", error);
+//     throw error;
+//   }
+// }
+
+// export default connectDB;
+
+//Conexion para Vercel
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
@@ -8,7 +37,7 @@ async function connectDB() {
   try {
     const connection = await mongoose.connect(mongoURI);
     console.log(
-      "MongoDB (Auth Service) conected to:",
+      "MongoDB (Auth Service) connected to:",
       connection.connection.host,
       connection.connection.port,
       connection.connection.name
