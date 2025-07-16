7217569a05763a670593176d295862271ff03ad8
Silrojua22 <silviojuarez60@gmail.com>
2025-07-15 22:44:20 -0300

    Deploy membership service

--- membership-service/src/config/database.js
+++ membership-service/src/config/database.js
@@ -1,39 +1,14 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-async function connectDB() {
-  dotenv.config();
-  const mongoURI = process.env.MONGO_URI_AUTH;
-
-  try {
-    const connection = await mongoose.connect(mongoURI);
-    console.log(
-      "MongoDB (Auth Service) conected to:",
-      connection.connection.host,
-      connection.connection.port,
-      connection.connection.name
-    );
-    return connection.connection;
-  } catch (error) {
-    console.error("MongoDB connection error (Auth Service):", error);
-    throw error;
-  }
-}
-
-export default connectDB;
-
-//Conexion para Vercel
 // import mongoose from "mongoose";
+// import dotenv from "dotenv";
 
 // async function connectDB() {
+//   dotenv.config();
 //   const mongoURI = process.env.MONGO_URI_AUTH;
 
 //   try {
-//     const connection = await mongoose.connect(mongoURI, {
-//       serverSelectionTimeoutMS: 30000,
-//     });
+//     const connection = await mongoose.connect(mongoURI);
 //     console.log(
-//       "MongoDB (Auth Service) connected to:",
+//       "MongoDB (Auth Service) conected to:",
 //       connection.connection.host,
 //       connection.connection.port,
 //       connection.connection.name
@@ -46,3 +21,28 @@ export default connectDB;
 // }
 
 // export default connectDB;
+
+//Conexion para Vercel
+import mongoose from "mongoose";
+
+async function connectDB() {
+  const mongoURI = process.env.MONGO_URI_AUTH;
+
+  try {
+    const connection = await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: 30000,
+    });
+    console.log(
+      "MongoDB (Auth Service) connected to:",
+      connection.connection.host,
+      connection.connection.port,
+      connection.connection.name
+    );
+    return connection.connection;
+  } catch (error) {
+    console.error("MongoDB connection error (Auth Service):", error);
+    throw error;
+  }
+}
+
+export default connectDB;
