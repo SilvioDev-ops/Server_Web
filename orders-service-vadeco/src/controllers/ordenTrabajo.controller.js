9e100460dd15ac1079b3ade03341abb9b2e30b50
Silvio Juarez <silviojuarez60@gmail.com>
2026-08-01 12:12:52 -0300

    asignar

--- orders-service-vadeco/src/controllers/ordenTrabajo.controller.js
+++ orders-service-vadeco/src/controllers/ordenTrabajo.controller.js
@@ -1,6 +1,5 @@
 import OrdenTrabajo from "../models/OrdenTrabajo.js";
 import Cuadrilla from "../models/Cuadrilla.js";
-
 const buildFilters = (query) => {
   const { estado, cuadrilla, localidad, empresa, fecha, desde, hasta, search } =
     query;
